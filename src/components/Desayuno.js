999d32ed07a36bff5f2f7e16694ef7f5c34fa223
Lidianys <lidianysrodriguez96@gmail.com>
2022-05-03 20:18:18 -0400

    se agregan los elementos

--- src/components/Desayuno.js
+++ src/components/Desayuno.js
@@ -1,15 +1,46 @@
 import data from '../data/menu.json';
 import '../css/css-components/menu.css';
+import { useState } from 'react';
+// import Cuenta from '../components/Cuenta';
 export default function Desayuno() {
+    
+    const [cart, setCart] = useState([]);
+
+
+    //función para agregar elementos 
+    function addToCart(item) {
+        setCart([...cart, { ...item }]);
+        console.log("estamos dando click a los elementos");
+    }
+
+    //FUNCION PARA ELIMINAR ITEMS DEL CARRITO
+    const removeFromCart = (productToRemove) => {
+    setCart(cart.filter((item) => item !== productToRemove))
+    console.log('estamos borrando');
+  }
+
     return (
         <div className="container-menu">
-            {data.desayunos.map(element => {
+            {data.desayunos.map((item) => {
                 return (
-                    <div  className= "container-products" key={element.id}>
-                    <p className="nombre-producto"> {element.name} </p>
-                    <p className="precio"> {element.price}  </p>
+                    <div className="container-products" key={item.id} onClick={() => addToCart(item)} >
+                        <p className="nombre-producto"> {item.name}  </p>
+                        <p className="precio"> {item.price} </p>
                     </div>)
 
             })}
-        </div>)
+            <div>
+                {cart.map((item) => {
+                    return (
+                        <div className='items-del-pedido' key={item.name}>
+                            {item.name}
+                            {item.price}
+                            <button onClick={() => removeFromCart(item)}> <p>Eliminar</p></button>
+                        </div>);
+                }
+             )}
+            </div>
+       
+</div>)
+
 }
